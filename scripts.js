45aa605bfe0d8adbf3cc2ce3ac01de22c7595021
jayagz <jayaguantz@gmail.com>
2023-11-11 21:58:58 +0800

    added scripts

---
+++ scripts.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Change Background Color</title>
+    <link rel="stylesheet" href="styles.css">
+    <script>
+        function changeBackgroundColor() {
+            document.body.style.backgroundColor = "#ff0000"; // Change to desired color
+        }
+    </script>
+</head>
+<body>
+    <h1>Change Background Color</h1>
+    <p>Click the button below to change the background color of the page.</p>
+    <button onclick="changeBackgroundColor()">Change Color</button>
+</body>
+</html>
